a4c6e89ef8d21d5e871325c56fbaf73d31e0d99e
Andrés Londoño <andreslon@outlook.com>
2019-03-04 12:08:55 -0500

    Se cambia proyecto por tabs

--- Xamarin/Xamarin.App/Xamarin.App/App.xaml.cs
+++ Xamarin/Xamarin.App/Xamarin.App/App.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using Xamarin.App.Services;
 using Xamarin.App.Views;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
@@ -9,18 +8,11 @@ namespace Xamarin.App
 {
     public partial class App : Application
     {
-        //TODO: Replace with *.azurewebsites.net url after deploying backend to Azure
-        public static string AzureBackendUrl = "http://localhost:5000";
-        public static bool UseMockDataStore = true;
 
         public App()
         {
             InitializeComponent();
 
-            if (UseMockDataStore)
-                DependencyService.Register<MockDataStore>();
-            else
-                DependencyService.Register<AzureDataStore>();
 
             MainPage = new MainPage();
         }
